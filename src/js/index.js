97562cdd51f80821c5888180905f1edb6d7ca06e
Michal Szymanski <m.szymanski@mdbootstrap.com>
2025-03-20 15:11:03 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+      // Import necessary TW Elements components
+      import {Tab,Tooltip,Ripple,Collapse, initTWE } from "tw-elements";
+
+      // Initialize TW Elements
+      initTWE({Tab, Tooltip,Ripple,Collapse });
